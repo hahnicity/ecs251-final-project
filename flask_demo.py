5ca9ea5e9193c8dd348ca0e13d112eefb10a1429
Greg Rehm <grehm87@gmail.com>
2016-05-25 16:11:09 -0700

    debug output

--- flask_demo.py
+++ flask_demo.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print(type(request.data))
+    print(request.data)
     data = json.loads(request.data)
+    print(type(data))
     breath_data = data["breath_data"]
     print(breath_data)
     return json.dumps(data)
